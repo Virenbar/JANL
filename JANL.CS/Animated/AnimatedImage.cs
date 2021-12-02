7f5b2ab3ef962c92d20bc553979eda41b6b745c9
Virenbar <rib.artem@gmail.com>
2021-12-02 18:45:08 +0500

    WIP

---
+++ JANL.CS/Animated/AnimatedImage.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JANL.Animators
+{
+	public abstract class AnimatedImage
+	{
+		protected int Delay;
+		protected int Framerate;
+
+		private readonly Control Parent;
+		private bool Animated;
+
+		public AnimatedImage(Control Parent) : this(Parent, new Bitmap(16, 16)) { }
+
+		public AnimatedImage(Control Parent, Image Image)
+		{
+			Framerate = 30;
+			Delay = 1000 / Framerate;
+			this.Parent = Parent;
+			this.Parent.Paint += Parent_Paint;
+			this.Image = Image;
+			Size = Image.Size;
+		}
+
+		public abstract void ResetAnimation();
+
+		public virtual void SetFramerate(int Framerate)
+
+		{
+			Delay = 1000 / Framerate;
+			this.Framerate = Framerate;
+		}
+
+		public void StartAnimation()
+		{
+			if (Animated) { return; }
+			Animated = true;
+			_ = AnimationLoop();
+		}
+
+		public void StopAnimation() => Animated = false;
+
+		protected abstract void PaintFrame(Graphics G);
+
+		protected abstract void PrepareFrame();
+
+		private async Task AnimationLoop()
+		{
+			while (Animated)
+			{
+				await Task.Delay(Delay);
+				PrepareFrame();
+				//Region R = new Region(new Rectangle(Parent.Width / 2, 0, Parent.Width, Parent.Height));
+				//Parent.SetBounds(Parent.Left, Parent.Top, Parent.Width, Parent.Height, BoundsSpecified.None);
+				//Parent.Invalidate(R, true);
+				OnFramePrepared();
+			}
+		}
+
+		public void Paint(Graphics G) => PaintFrame(G);
+
+		private void Parent_Paint(object sender, PaintEventArgs e)
+		{
+			//PaintFrame(e.Graphics);
+		}
+
+		#region Properties
+		protected int Height => Parent.Height;
+		public Image Image { get; set; }
+		protected int Width => Parent.Width;
+		public Size Size { get; protected set; }
+
+		public Rectangle Bounds => new Rectangle(Position, Size);
+		public Point Position { get; set; }
+
+		#endregion Properties
+
+		#region Events
+
+		protected void OnFramePrepared() => FramePrepared?.Invoke(this, EventArgs.Empty);
+
+		public event EventHandler FramePrepared;
+
+		#endregion Events
+	}
+}
---
+++ JANL.CS/Animated/RotatingImage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JANL.Animators
+{
+	public class RotatingImage : AnimatedImage
+	{
+		private float Angle;
+		private float AngleStep;
+		private int Duration;
+		private const int Total = 360;
+
+		public RotatingImage(Control Parent, Image Image) : this(Parent, Image, 5 * 1000) { }
+
+		public RotatingImage(Control Parent, Image Image, int Duration) : base(Parent, Image)
+		{
+			Angle = 0;
+			SetDuration(Duration);
+			Direction = DirectionType.Clockwise;
+			Size = new Size((int)Math.Ceiling(Image.Width * 1.707), (int)Math.Ceiling(Image.Height * 1.707));
+		}
+
+		public DirectionType Direction { get; set; }
+
+		public override void ResetAnimation() { Angle = 0; }
+
+		public void SetDuration(int Duration)
+		{
+			this.Duration = Duration;
+			AngleStep = Total / ((float)Duration / Delay);
+		}
+
+		public override void SetFramerate(int Framerate)
+		{
+			base.SetFramerate(Framerate);
+			SetDuration(Duration);
+		}
+
+		protected override void PaintFrame(Graphics G)
+		{
+			G.TranslateTransform((float)Width / 2, (float)Height / 2);
+			G.RotateTransform(Angle);
+
+			//G.TranslateTransform(-(float)Width / 2, -(float)Height / 2);
+
+			G.TranslateTransform(-(float)Image.Width / 2, -(float)Image.Height / 2);
+
+			G.DrawImage(Image, new Point(0, 0));
+		}
+
+		protected override void PrepareFrame()
+		{
+			Angle = (Angle + (int)Direction * AngleStep) % Total;
+		}
+
+		public enum DirectionType : int
+		{
+			Clockwise = 1,
+			Counterclockwise = -1
+		}
+	}
+}
