bc3a4bb33fb30b63534babd94ba7901c0269e8f9
Virenbar <rib.artem@gmail.com>
2022-01-05 14:59:30 +0500

    WIP

--- JANL.CS/SQL/SelectProcedure.cs
+++ JANL.CS/SQL/SelectProcedure.cs
@@ -5,101 +5,82 @@ using System.Threading.Tasks;
 
 namespace JANL.SQL
 {
-	/// <summary>
-	/// Процедура возвращающая <see cref="DataTable"/>
-	/// </summary>
-	public class SelectProcedure : SQLProcedure<DataTable>
-	{
-		/// <summary>
-		/// Создаёт новую процедуру именем вызывающего метода
-		/// </summary>
-		public SelectProcedure([CallerMemberName] string Name = null) : base(Name) { }
+    /// <summary>
+    /// Процедура возвращающая <see cref="DataTable"/>
+    /// </summary>
+    public class SelectProcedure : SQLProcedure<DataTable>
+    {
+        /// <summary>
+        /// Создаёт новую процедуру именем вызывающего метода
+        /// </summary>
+        public SelectProcedure([CallerMemberName] string Name = null) : base(Name) { }
 
-		/// <summary>
-		/// Выполнить с указанным соединением
-		/// </summary>
-		public override DataTable Execute(SqlConnection Connection)
-		{
-			var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
-			SQLCommand.Connection = Connection;
-			using (var R = SQLCommand.ExecuteReader())
-			{
-				Result.Load(R);
-			}
-			return Result;
-		}
+        /// <summary>
+        /// Выполнить с указанным соединением
+        /// </summary>
+        public override DataTable Execute(SqlConnection Connection)
+        {
+            var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
+            SQLCommand.Connection = Connection;
+            using (var R = SQLCommand.ExecuteReader())
+            {
+                Result.Load(R);
+            }
+            return Result;
+        }
 
-		/// <summary>
-		/// Асинхронно выполнить с указанным соединением
-		/// </summary>
-		public override async Task<DataTable> ExecuteAsync(SqlConnection Connection)
-		{
-			var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
-			SQLCommand.Connection = Connection;
-			using (var R = await SQLCommand.ExecuteReaderAsync())
-			{
-				await Task.Run(() => Result.Load(R));
-			}
-			return Result;
-		}
-	}
-	/// <summary>
-	/// Процедура возвращающая <see cref="T"/>
-	/// </summary>
-	public abstract class SelectProcedure<T> : SQLProcedure<T>
-	{
-		/// <summary>
-		/// Создаёт новую процедуру именем вызывающего метода
-		/// </summary>
-		public SelectProcedure(string Name) : base(Name) { }
+        /// <summary>
+        /// Асинхронно выполнить с указанным соединением
+        /// </summary>
+        public override async Task<DataTable> ExecuteAsync(SqlConnection Connection)
+        {
+            var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
+            SQLCommand.Connection = Connection;
+            using (var R = await SQLCommand.ExecuteReaderAsync())
+            {
+                await Task.Run(() => Result.Load(R));
+            }
+            return Result;
+        }
+    }
+    /// <summary>
+    /// Процедура возвращающая <see cref="T"/>
+    /// </summary>
+    internal abstract class SelectProcedure<T> : SQLProcedure<T>
+    {
+        /// <summary>
+        /// Создаёт новую процедуру именем вызывающего метода
+        /// </summary>
+        public SelectProcedure(string Name) : base(Name) { }
 
-		/// <summary>
-		/// Выполнить с указанным соединением
-		/// </summary>
-		public override T Execute(SqlConnection Connection)
-		{
-			var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
-			SQLCommand.Connection = Connection;
-			using (var R = SQLCommand.ExecuteReader())
-			{
-				Result.Load(R);
-			}
-			return Cast(Result);
-		}
+        /// <summary>
+        /// Выполнить с указанным соединением
+        /// </summary>
+        public override T Execute(SqlConnection Connection)
+        {
+            var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
+            SQLCommand.Connection = Connection;
+            using (var R = SQLCommand.ExecuteReader())
+            {
+                Result.Load(R);
+            }
+            return Cast(Result);
+        }
 
-		/// <summary>
-		/// Асинхронно выполнить с указанным соединением
-		/// </summary>
-		public override async Task<T> ExecuteAsync(SqlConnection Connection)
-		{
-			var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
-			SQLCommand.Connection = Connection;
-			using (var R = await SQLCommand.ExecuteReaderAsync())
-			{
-				await Task.Run(() => Result.Load(R));
-			}
-			return Cast(Result);
-		}
+        /// <summary>
+        /// Асинхронно выполнить с указанным соединением
+        /// </summary>
+        public override async Task<T> ExecuteAsync(SqlConnection Connection)
+        {
+            var Result = new DataTable() { Locale = System.Globalization.CultureInfo.CurrentCulture };
+            SQLCommand.Connection = Connection;
+            using (var R = await SQLCommand.ExecuteReaderAsync())
+            {
+                await Task.Run(() => Result.Load(R));
+            }
+            return Cast(Result);
+        }
 
-		protected abstract T Cast(DataTable DT);
-	}
-
-	internal class SSS : SelectProcedure<TTT>
-	{
-		public SSS() : base(nameof(SSS)) { }
-
-		protected override TTT Cast(DataTable DT)
-		{
-			throw new System.NotImplementedException();
-		}
-	}
-	internal class TTT { }
-
-	public class AAA
-	{
-		public static void A()
-		{
-			var P = new SSS();
-		}
-	}
+        protected abstract T Cast(DataTable DT);
+    }
 }
