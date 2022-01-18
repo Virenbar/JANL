d70ac983747c787da5a2f5cd1eeb96f29001757a
Virenbar <rib.artem@gmail.com>
2022-01-18 10:31:52 +0500

    WIP

--- JANL/DataTableStreamer.cs
+++ JANL/DataTableStreamer.cs
@@ -8,57 +8,72 @@ using System.Data;
 
 namespace JANL
 {
-	public class DataTableStreamer : IDisposable
-	{
-		private readonly SqlBulkCopy SBC;
-		private readonly SqlConnection Source;
-		private readonly string Table;
-		private readonly SqlConnection Target;
+    public class DataTableStreamer : IDisposable
+    {
+        private readonly IProgress<long> Progress;
+        private readonly SqlBulkCopy SBC;
+        private readonly SqlConnection Source;
+        private readonly string SourceTable;
+        private readonly SqlConnection Target;
+        private readonly string TargetTable;
 
-		public DataTableStreamer(string source, string target, string table)
-		{
-			Source = SQL.SQLHelper.NewConnection(source);
-			Target = SQL.SQLHelper.NewConnection(target);
-			Table = table;
-			SBC = new SqlBulkCopy(target, SqlBulkCopyOptions.KeepIdentity)
-			{
-				EnableStreaming = true
-			};
-		}
+        public DataTableStreamer(string source, string target, string table, IProgress<long> progress) : this(source, target, table, table) { }
 
-		public void Copy()
-		{
-			var SDR = GetReader(Source, Table);
-			SBC.WriteToServer(SDR);
-		}
+        public DataTableStreamer(string source, string target, string sourceTable, string targetTable, IProgress<long> progress) : this(source, target, sourceTable, targetTable)
+        {
+            Progress = progress;
+            SBC.SqlRowsCopied += (object sender, SqlRowsCopiedEventArgs e) => Progress.Report(e.RowsCopied);
+        }
 
-		private static SqlDataReader GetReader(SqlConnection connection, string table)
-		{
-			var C = new SqlCommand($"SELECT * FROM {table}") { CommandType = CommandType.Text };
-			C.Connection = connection;
-			return C.ExecuteReader();
-		}
+        public DataTableStreamer(string source, string target, string table) : this(source, target, table, table) { }
 
-		#region IDisposable Support
-		private bool disposedValue = false; // Для определения избыточных вызовов
+        public DataTableStreamer(string source, string target, string sourceTable, string targetTable)
+        {
+            Source = SQL.SQLHelper.NewConnection(source);
+            Target = SQL.SQLHelper.NewConnection(target);
+            SourceTable = sourceTable;
+            TargetTable = targetTable;
+            SBC = new SqlBulkCopy(target, SqlBulkCopyOptions.KeepIdentity)
+            {
+                DestinationTableName = TargetTable,
+                NotifyAfter = 100,
+                EnableStreaming = true
+            };
+        }
 
-		void IDisposable.Dispose() => Dispose(true);
+        public void Copy()
+        {
+            var SDR = GetReader(Source, SourceTable);
+            SBC.WriteToServer(SDR);
+        }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposedValue)
-			{
-				if (disposing)
-				{
-					Source.Dispose();
-					Target.Dispose();
-					SBC.Close();
-				}
+        private static SqlDataReader GetReader(SqlConnection connection, string table)
+        {
+            var C = new SqlCommand($"SELECT * FROM {table}") { CommandType = CommandType.Text };
+            C.Connection = connection;
+            return C.ExecuteReader();
+        }
 
-				disposedValue = true;
-			}
-		}
+        #region IDisposable Support
+        private bool disposedValue = false; // Для определения избыточных вызовов
 
-		#endregion IDisposable Support
-	}
+        void IDisposable.Dispose() => Dispose(true);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Source.Dispose();
+                    Target.Dispose();
+                    SBC.Close();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        #endregion IDisposable Support
+    }
 }
