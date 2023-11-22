8a5ad7b13041a15bf4af98f23f8ff9799951afe5
Virenbar <rib.artem@gmail.com>
2023-11-22 17:18:40 +0500

    chore: small fixes

--- JANL.WinForms/ExtendedControls/ToolStripTextBoxLabel.cs
+++ JANL.WinForms/ExtendedControls/ToolStripTextBoxLabel.cs
@@ -13,17 +13,17 @@ namespace JANL
         {
             ForeColor = Color.Black;
             LabelColor = Color.SlateGray;
-            ToolTipText = "Esc";
+            ToolTipText = "Esc - очистить фильтр";
             Timer.Tick += Timer_Tick;
         }
-
-        private void Timer_Tick(object sender, EventArgs e) => CompleteInput();
 
         private void CompleteInput()
         {
             Timer.Stop();
             OnInputDone(EventArgs.Empty);
         }
+
+        private void Timer_Tick(object sender, EventArgs e) => CompleteInput();
 
         private void UpdateColor() => base.ForeColor = TextValue?.Length > 0 ? ForeColor : LabelColor;
 
@@ -32,13 +32,6 @@ namespace JANL
         #region Designer
         private string LabelValue = "";
         private string TextValue = "";
-
-        [Browsable(true), Category("TextBoxLabel"), DefaultValue(1000)]
-        public int WaitTime
-        {
-            get => Timer.Interval;
-            set => Timer.Interval = value;
-        }
 
         [Browsable(true), Category("TextBoxLabel"), DefaultValue(typeof(Color), "0x000000")]
         public new Color ForeColor { get; set; }
@@ -51,7 +44,7 @@ namespace JANL
             {
                 if (LabelValue == value) { return; }
                 LabelValue = value;
-                if (TextValue.Length == 0) { base.Text = LabelValue; }
+                if (TextValue?.Length == 0) { base.Text = LabelValue; }
                 UpdateColor();
             }
         }
@@ -72,14 +65,48 @@ namespace JANL
             }
         }
 
+        [Browsable(true), Category("TextBoxLabel"), DefaultValue(1000)]
+        public int WaitTime
+        {
+            get => Timer.Interval;
+            set => Timer.Interval = value;
+        }
+
         #endregion Designer
 
         [Browsable(false)]
-        public bool IsLabel { get; private set; }
+        public bool IsLabel => TextValue?.Length == 0;
 
         #endregion Properties
 
         #region Overrides
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.Text = TextValue;
+            base.OnGotFocus(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            Timer.Stop();
+            Timer.Start();
+            base.OnKeyDown(e);
+        }
+
+        protected override void OnLostFocus(EventArgs e)
+        {
+            if (base.Text.Length == 0) { base.Text = Label; }
+            CompleteInput();
+            base.OnLostFocus(e);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            if (Focused) { TextValue = base.Text; }
+            UpdateColor();
+            base.OnTextChanged(e);
+        }
 
         protected override bool ProcessCmdKey(ref Message m, Keys keyData)
         {
@@ -98,40 +125,13 @@ namespace JANL
             }
         }
 
-        protected override void OnGotFocus(EventArgs e)
-        {
-            base.Text = TextValue;
-            base.OnGotFocus(e);
-        }
-
-        protected override void OnLostFocus(EventArgs e)
-        {
-            if (base.Text.Length == 0) { base.Text = Label; }
-            CompleteInput();
-            base.OnLostFocus(e);
-        }
-
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            Timer.Stop();
-            Timer.Start();
-            base.OnKeyDown(e);
-        }
-
-        protected override void OnTextChanged(EventArgs e)
-        {
-            if (Focused) { TextValue = base.Text; }
-            UpdateColor();
-            base.OnTextChanged(e);
-        }
-
         #endregion Overrides
 
         #region Events
 
-        public event EventHandler InputDone;
-
         protected void OnInputDone(EventArgs e) => InputDone?.Invoke(this, e);
+
+        public event EventHandler InputDone;
 
         #endregion Events
     }
