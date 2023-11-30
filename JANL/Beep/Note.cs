e98211469fb61fb6dc5249d3710daff0c2cd28a5
Virenbar <rib.artem@gmail.com>
2023-11-30 16:30:39 +0500

    feat: beep song

---
+++ JANL/Beep/Note.cs
@@ -0,0 +1,116 @@
+﻿using JANL.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace JANL.Beep
+{
+    public struct Note
+    {
+        private const int A440 = 440;
+        private readonly static Dictionary<string, int> NoteNumber = new Dictionary<string, int>();
+        private readonly static Dictionary<string, double> NoteValues = new Dictionary<string, double>();
+        private static Regex RNote = new Regex(@"(?<Pitch>[A-G][#b]?|R)(?<Octave>[0-8])?(?<Value>[WHQES]\.{0,3})?", RegexOptions.IgnoreCase);
+        private static Regex RNumber = new Regex(@"(?<Pitch>[A-G][#bn]?|R)(?<Octave>[0-8])?", RegexOptions.IgnoreCase & RegexOptions.Compiled);
+        private static Regex RNoteSS = new Regex(@"(?<Notes>(?>[A-GR][#bn]?[0-8]?-?)+)\/?(?<Value>\d.\d+)?", RegexOptions.IgnoreCase);
+        //(?<Note>((?<Pitch>[A-G][#b]?|R)(?<Octave>[0-8])?-?)+)(?<Value>\/\d.\d+)?
+
+        static Note()
+        {
+            NoteNumber.Add("R", 0);
+            NoteNumber.Add("C", 60);
+            NoteNumber.Add("C#", 61); NoteNumber.Add("Db", 61);
+            NoteNumber.Add("D", 62);
+            NoteNumber.Add("D#", 63); NoteNumber.Add("Eb", 63);
+            NoteNumber.Add("E", 64);
+            NoteNumber.Add("F", 65);
+            NoteNumber.Add("F#", 66); NoteNumber.Add("Gb", 66);
+            NoteNumber.Add("G", 67);
+            NoteNumber.Add("G#", 68); NoteNumber.Add("Ab", 68);
+            NoteNumber.Add("A", 69);
+            NoteNumber.Add("A#", 70); NoteNumber.Add("Bb", 70);
+            NoteNumber.Add("B", 71);
+
+            var names = new[] { "W", "H", "Q", "E", "S" };
+            var initial = 1d;
+            foreach (var name in names)
+            {
+                var value = 0d;
+                for (int i = 0; i <= 3; i++)
+                {
+                    value += initial / Math.Pow(2, i);
+                    NoteValues.Add(name + new string('.', i), value);
+                }
+                initial /= 2;
+            }
+        }
+
+        public Note(double value)
+        {
+            Value = value;
+            Frequency = 0;
+            Number = 0;
+            Octave = 0;
+        }
+
+        public Note(NoteName note, double value) : this((int)note, 4, value) { }
+
+        public Note(NoteName note, int octave, double value) : this((int)note, octave, value) { }
+
+        public Note(int note, int octave, double value) : this(value)
+        {
+            if (note == 0) { return; }
+            Octave = octave;
+            Number = note - 12 * (4 - octave);
+            Frequency = (int)(A440 * Math.Pow(2, (Number - 69) / 12d));
+        }
+
+        public int Frequency { get; private set; }
+        public int Number { get; private set; }
+        public int Octave { get; private set; }
+        public double Value { get; private set; }
+
+        public static int GetDuration(int tempo, double value) => (int)(GetDuration(tempo) * value * 4);
+
+        public static int GetDuration(int tempo) => 60_000 / tempo;
+
+        public static Note Parse(string note)
+        {
+            var M = RNote.Match(note);
+            var pitch = M.Groups["Pitch"].Value;
+            var octave = M.Groups["Octave"].Value.NullIfEmpty() ?? "4";
+            var value = M.Groups["Value"].Value.NullIfEmpty() ?? "Q";
+
+            var N = new Note(NoteNumber[pitch], int.Parse(octave), NoteValues[value]);
+            return N;
+        }
+
+        public static int ParseNumber(string note)
+        {
+            var M = RNumber.Match(note);
+            var pitch = M.Groups["Pitch"].Value;
+            var octave = M.Groups["Octave"].Value.NullIfEmpty() ?? "4";
+
+            return NoteNumber[pitch] - 12 * (4 - int.Parse(octave));
+        }
+
+        public int Duration(int tempo) => GetDuration(tempo, Value);
+    }
+
+    public enum NoteName
+    {
+        REST = 0,
+        C = 60,
+        C_D = 61,
+        D = 62,
+        D_E = 63,
+        E = 64,
+        F = 65,
+        F_G = 66,
+        G = 67,
+        G_A = 68,
+        A = 69,
+        A_B = 70,
+        B = 71
+    }
+}
---
+++ JANL/Beep/Song.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JANL.Beep
+{
+    public class Song
+    {
+        private static readonly Regex RBPM = new Regex(@"(?>BPM=)\d+", RegexOptions.IgnoreCase);
+        public bool Playing { get; protected set; }
+        public List<Note> Notes { get; } = new List<Note>();
+        public int Tempo { get; private set; } = 150;
+
+        public static Song Parse(string song, int tempo)
+        {
+            var S = Parse(song);
+            S.Tempo = tempo;
+            return S;
+        }
+
+        public static Song Parse(string song)
+        {
+            var S = new Song();
+            var notes = song.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var note in notes)
+            {
+                var BPM = RBPM.Match(note);
+                if (BPM.Success)
+                {
+                    S.Tempo = int.Parse(BPM.Groups[0].Value);
+                    continue;
+                }
+                S.Notes.Add(Note.Parse(note));
+            }
+            return S;
+        }
+
+        public static Song Parse(IEnumerable<string> song) => Parse(string.Join(",", song));
+
+        public static Song Parse(IEnumerable<string> song, int tempo) => Parse(string.Join(",", song), tempo);
+
+        public void Play()
+        {
+            if (Playing) { return; }
+            Playing = true;
+            foreach (var note in Notes)
+            {
+                if (!Playing) { return; }
+                if (note.Frequency < 37)
+                {
+                    Thread.Sleep(note.Duration(Tempo));
+                }
+                else
+                {
+                    Console.Beep(note.Frequency, note.Duration(Tempo));
+                }
+            }
+            Playing = false;
+        }
+
+        public async Task PlayAsync(CancellationToken CT)
+        {
+            if (Playing) { return; }
+            Playing = true;
+            foreach (var note in Notes)
+            {
+                if (!Playing) { return; }
+                CT.ThrowIfCancellationRequested();
+                if (note.Frequency < 37)
+                {
+                    await Task.Delay(note.Duration(Tempo));
+                }
+                else
+                {
+                    await Task.Run(() => Console.Beep(note.Frequency, note.Duration(Tempo)));
+                }
+            }
+            Playing = false;
+        }
+
+        public void Stop() => Playing = false;
+    }
+}
