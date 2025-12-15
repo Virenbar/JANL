3258e4008e8ecae1b656c3f64c27c07673bd7c2b
Virenbar <rib.artem@gmail.com>
2025-12-15 16:55:45 +0500

    chore: update workflow

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyDescription("Just Another .NET Library")]
 [assembly: AssemblyCompany("Virenbar")]
 [assembly: AssemblyProduct("JANL")]
-[assembly: AssemblyCopyright("Copyright © Virenbar 2024")]
+[assembly: AssemblyCopyright("Copyright © Virenbar 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 //
