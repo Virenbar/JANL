09e3c9a9f9d15f0d1a6641cac3538433695b6e42
Virenbar <rib.artem@gmail.com>
2026-07-06 18:01:19 +0500

    chore: update TemplateDocument

--- JANL.Office/Word/TemplateDocument.cs
+++ JANL.Office/Word/TemplateDocument.cs
@@ -5,60 +5,119 @@ using System.IO;
 namespace JANL.Word
 {
     /// <summary>
-    ///
+    /// Базовый класс шаблона документа
     /// </summary>
     public abstract class TemplateDocument : IDisposable
     {
-        protected FileInfo _file;
-        private readonly Stream _stream;
-        private readonly WordprocessingDocument _template;
+        /// <summary>
+        /// Шаблон документа в виде байт
+        /// </summary>
+        protected byte[] _buffer;
 
+        /// <summary>
+        /// Файл для сохранения
+        /// </summary>
+        protected FileInfo _file;
+
+        private Stream _stream;
+        private WordprocessingDocument _template;
+
+        /// <summary>
+        /// Создает пустой шаблон
+        /// </summary>
         protected TemplateDocument()
         {
             _file = new FileInfo(Path.GetTempFileName().Replace("tmp", "docx"));
         }
 
-        protected TemplateDocument(Stream Stream) : this()
+        /// <summary>
+        /// Создает шаблон на основе потока
+        /// </summary>
+        /// <param name="stream"></param>
+        protected TemplateDocument(Stream stream) : this()
         {
-            _template = WordprocessingDocument.Open(Stream, false);
+            _stream = stream;
         }
 
-        protected TemplateDocument(byte[] Buffer) : this()
+        /// <summary>
+        /// Создает шаблон на основе буфера
+        /// </summary>
+        /// <param name="buffer"></param>
+        protected TemplateDocument(byte[] buffer) : this()
         {
-            _stream = new MemoryStream(Buffer);
-            _template = WordprocessingDocument.Open(_stream, false);
+            _buffer = buffer;
         }
 
+        /// <summary>
+        /// Формирует документ и сохраняет во временный файл
+        /// </summary>
         public FileInfo Print()
         {
             Print(_file);
             return _file;
         }
 
-        public FileInfo Print(string FileName)
+        /// <summary>
+        /// Формирует документ и сохраняет его по пути <paramref name="fileName"/>
+        /// </summary>
+        /// <param name="fileName">Путь для сохранения</param>
+        public FileInfo Print(string fileName)
         {
-            _file = new FileInfo(FileName);
+            _file = new FileInfo(fileName);
             Print(_file);
             return _file;
         }
 
+        /// <summary>
+        /// Формирует документ и сохраняет в файл <paramref name="file"/>
+        /// </summary>
+        /// <param name="file">Файл для сохранения</param>
         public void Print(FileInfo file)
         {
             if (!file.Directory.Exists) { file.Directory.Create(); }
-            using (var document = (WordprocessingDocument)_template.Clone(file.FullName, true))
+            _template = CreateDocumentTemplate();
+            using (var document = _template.Clone(file.FullName, true))
             {
                 PrintDocument(document);
                 document.Save();
             }
         }
 
+        /// <summary>
+        /// Создает шаблон на основе буфера
+        /// </summary>
+        protected virtual WordprocessingDocument CreateDocumentTemplate()
+        {
+            if (_stream is null)
+            {
+                _stream = new MemoryStream(_buffer);
+            }
+            return WordprocessingDocument.Open(_stream, false);
+        }
+
+        /// <summary>
+        /// Метод заполнения документа
+        /// </summary>
+        /// <param name="document">Шаблон документа</param>
         protected abstract void PrintDocument(WordprocessingDocument document);
+
+        /// <summary>
+        /// Задает шаблон документа
+        /// </summary>
+        /// <param name="buffer"></param>
+        protected void SetTemplate(byte[] buffer)
+        {
+            _buffer = buffer;
+        }
 
         #region Dispose
 
+        /// <summary>
+        ///
+        /// </summary>
         public void Dispose()
         {
-            _template.Dispose();
+            _template?.Dispose();
             _stream?.Dispose();
         }
 
